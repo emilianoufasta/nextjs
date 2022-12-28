9f1c5f5eec1b43a979b5940b5e1bba0bce1d6390
Emiliano Belvedere <ebelverde@godaddy.com>
2022-12-28 12:33:04 -0300

    fix pr comments

--- src/components/cats/cats.tsx
+++ src/components/cats/cats.tsx
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCatsFetch } from "../../redux/catsState";
 
-import { Spinner } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import { List } from "../list/list";
 import { catsSelector } from "../../redux/catsSelector";
 
 export const CatsList = () => {
   const dispatch = useDispatch();
+
+  const [ color, setColor ] = useState('green');
+
   useEffect(()=>{
     dispatch(getCatsFetch());
   }, [dispatch])
 
   const { isLoading, data } = useSelector(catsSelector);
+
+  const miBoton = <Button onClick={() => { setColor('blue'); } } />;
+
   return (
     <div className='p-4 row d-flex align-item-center justify-content-between'>
-      { isLoading && <Spinner animation="grow" variant="primary" />}
+      { isLoading && <Spinner animation="grow" variant="primary" /> }
       { !isLoading && <List data={data} /> }
+      { color }
+      { miBoton }
     </div>
   )
-}
+};
--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -16,4 +16,4 @@ export const List = ({ data }) => {
       </Card.Body>
     </Card>
   ))
-}
+};
